f71112d21e65c985f24ec095005e0dbe4d6ca86c
AbhaySingh-33 <abhaysingh957152@gmail.com>
2025-12-16 13:03:53 +0530

    context api updated

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -6,11 +6,11 @@ import toast, { Toaster } from "react-hot-toast";
 import Cookies from "js-cookie";
 import axios from "axios";
 
-export const utils_service = "http://localhost:5001";
-export const auth_service = "http://localhost:5000";
-export const user_service = "http://localhost:5002";
-export const job_service = "http://localhost:5003";
-export const payment_service = "http://localhost:5004";
+export const utils_service = process.env.NEXT_PUBLIC_UTILS_SERVICE;
+export const auth_service = process.env.NEXT_PUBLIC_AUTH_SERVICE;
+export const user_service = process.env.NEXT_PUBLIC_USER_SERVICE;
+export const job_service = process.env.NEXT_PUBLIC_JOB_SERVICE;
+export const payment_service = process.env.NEXT_PUBLIC_PAYMENT_SERVICE;
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
